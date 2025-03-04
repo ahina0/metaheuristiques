cec975565990290c2a7dba93e0bf89f0e72fc0f4
MorganReyS <morgan.rey@etu.u-bordeaux.fr>
2025-03-04 23:11:26 +0100

    Add files via upload

---
+++ Glouton.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Solution.hpp"
+
+class Glouton : public Solution {
+    
+    public:
+        Glouton(Graph &G, int p);
+        
+        // Calcule la variation d'objectif d'un PnD
+        double delta_obj(int sommet, int classe_depart, int classe_arrivee);    
+
+        double gradient(int iter);
+
+        
+};
