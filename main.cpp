ea213fec4d94a11923455820604cb29bf1c0fc4e
MorganReyS <morgan.rey@etu.u-bordeaux.fr>
2025-03-10 21:42:21 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -22,81 +22,112 @@ int main(int argc, char ** argv){
     if (argc==3){
 
         int k = stoi(argv[2]);
-        string filename = string(argv[1]);
+        string filenameC = string(argv[1]);
         string file_results = "../results/";
-        file_results += string(argv[2]) + ".txt";
+        file_results += string(argv[2]) + "_classes.txt";
+        string file_enum = "../results/Enum_";
+        file_enum += string(argv[2]) + ".txt";
+        string file_grad = "../results/Grad_";
+        file_grad += string(argv[2]) + ".txt";
 
 
-        Graph G(filename);
+        Graph G(filenameC);
 
-        ofstream file(file_results, ios::app);
+        ofstream fileR(file_results, ios::app);
+        ofstream fileE(file_enum, ios::app);
+        ofstream fileG(file_grad, ios::app);
 
-        // Enumeration
-        file << "Enumeration :\n";
-        if (G.nbSommets > 15){
-            file << "Nombre de sommets trop grand pour l'énumération\n";
-        } else {
+
+        // Pour enlever le chemin du nom du fichier
+        size_t pos = filenameC.find_last_of("/\\");
+        string filename = (pos ==  string::npos) ? filenameC : filenameC.substr(pos + 1);
+        
+        
+
+        if (G.nbSommets > k){    
+            
+            // Enumeration
+            fileR << "Enumeration :\n";
+            if (pow(k,G.nbSommets) > 1.5e8){    // Condition sur le nombre de classes  
+                fileR << "Nombre de sommets trop grand pour l'énumération pour " << k << " classes\n";
+            } else {
+                fileE << filename << " " << G.nbSommets << " ";
+                t1 = steady_clock::now();
+                Enumeration E(G,k);
+                double min = E.explicite();
+                t2 = steady_clock::now();
+                s = duration_cast<milliseconds>(t2 - t1);
+                fileR << "Valeur optimale par énumération explicite pour " << E.nbClasses << " classes : " << min << "\n";
+                fileE << min << " ";
+                fileR << "temps en ms : " << s.count() << "\n";
+                fileE << s.count() << "\n";
+            }
+            fileR << "\n";
+
+
+            // Gradient
+            fileR << "Gradient :\n";
+            int iter_max = 500;
+            int iter_algo = 100;
+            Glouton Gl(G,k);
+
+            vector<double> optimaux(iter_algo);
+            double somme = 0;
+            fileG << filename << " " << G.nbSommets << " ";
             t1 = steady_clock::now();
-            Enumeration E(G,k);
-            double min = E.explicite();
+            for (int i=0; i<iter_algo; i++){
+                Glouton Gl(G,k);
+                optimaux[i] = Gl.gradient(iter_max);
+                somme += optimaux[i];
+            }
             t2 = steady_clock::now();
             s = duration_cast<milliseconds>(t2 - t1);
-            file << "Valeur optimale par énumération explicite pour " << E.nbClasses << " classes : " << min << "\n";
-            file << "temps en ms : " << s.count() << "\n";
-        }
-        file << "\n";
 
+            // Calculs statistiques
+            sort(optimaux.begin(), optimaux.end());
+            fileR << "Meilleur optimum local : " << optimaux[0] << "\n";
+            fileG << optimaux[0] << " ";
+            fileR << "Pire optimum local : " << optimaux[iter_algo - 1] << "\n";
+            //fileG << optimaux[iter_algo - 1] << " ";
+            fileR << "Moyenne : " << somme/iter_algo << "\n";
+            //fileG << somme/iter_algo << " ";
 
-        // Gradient
-        file << "Gradient :\n";
-        int iter_max = 100;
-        int iter_algo = 100;
-        Glouton Gl(G,k);
+            // Médiane
+            fileR << "Médiane : ";
+            if (iter_algo % 2 == 0){
+                fileR << (optimaux[iter_algo/2 - 1] + optimaux[iter_algo/2]) / 2. << "\n";
+                //fileG << (optimaux[iter_algo/2 - 1] + optimaux[iter_algo/2]) / 2. << " ";
+            } else {
+                fileR << optimaux[iter_algo/2] << "\n";
+                //fileG << optimaux[iter_algo/2] << " ";
+            }
 
-        vector<double> optimaux(iter_algo);
-        double somme = 0;
-        t1 = steady_clock::now();
-        for (int i=0; i<iter_algo; i++){
-            Glouton Gl(G,k);
-            optimaux[i] = Gl.gradient(iter_max);
-            somme += optimaux[i];
-        }
-        t2 = steady_clock::now();
-        s = duration_cast<milliseconds>(t2 - t1);
+            // Nombre de meilleurs opt locaux
+            int nbOpt = 1;
+            while(abs(optimaux[0] - optimaux[nbOpt]) < 1e-6){
+                nbOpt += 1;
+            }
+            fileR << "Meilleur optimum local trouvé " << nbOpt << " fois" << "\n";
+            //fileG << nbOpt << " ";
+            fileR << "temps en ms : " << s.count() << "\n";
+            fileG << s.count() << "\n";
 
-        // Calculs statistiques
-        sort(optimaux.begin(), optimaux.end());
-        file << "Meilleur optimum local : " << optimaux[0] << "\n";
-        file << "Pire optimum local : " << optimaux[iter_algo - 1] << "\n";
-        file << "Moyenne : " << somme/iter_algo << "\n";
-
-        // Médiane
-        file << "Médiane : ";
-        if (iter_algo % 2 == 0){
-            file << (optimaux[iter_algo/2 - 1] + optimaux[iter_algo/2]) / 2. << "\n";
         } else {
-            file << optimaux[iter_algo/2] << "\n";
+        fileR << "On ne traite pas les graphes en dessous de " << k+1 << " sommets pour " << k << " classes\n";
         }
-
-        // Nombre de meilleurs opt locaux
-        int nbOpt = 1;
-        while(abs(optimaux[0] - optimaux[nbOpt]) < 1e-6){
-            nbOpt += 1;
-        }
-        file << "Meilleur optimum local trouvé " << nbOpt << " fois" << "\n";
-        file << "temps en ms : " << s.count() << "\n";
-
-
 
 
 
     } else {
 
-        string filename = "test.txt";
+        string filename/* = "test.txt"*/;
 
         if (argc == 2){
             filename = argv[1];
-        }
+        } else {
+            cout << "Erreur : un argument requis (nom du fichier)." << endl;
+            abort();
+        } 
 
         Graph G(filename);
 
